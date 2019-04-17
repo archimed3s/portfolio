dc4383e6ac5620fe280b8e94231c26fe1e5cf32b
Heorhii <h30rhii@gmail.com>
2019-04-18 00:23:26 +0300

    Ready animation

--- src/scripts/webgl/WebGLView.js
+++ src/scripts/webgl/WebGLView.js
@@ -1,44 +1,22 @@
-import 'three';
-import { TweenLite } from 'gsap/TweenMax';
-
+import * as THREE from 'three';
 import InteractiveControls from './controls/InteractiveControls';
 import Particles from './particles/Particles';
 
-const glslify = require('glslify');
-
 export default class WebGLView {
 
-	constructor(app) {
-		this.app = app;
-
-		this.samples = [
-			'images/sample-01.png',
-			'images/sample-02.png',
-			'images/sample-03.png',
-			'images/sample-04.png',
-			'images/sample-05.png',
-		];
-
+	constructor() {
+		this.samples = 'images/sample-latest.png';
 		this.initThree();
 		this.initParticles();
 		this.initControls();
-
-		const rnd = ~~(Math.random() * this.samples.length);
-		this.goto(rnd);
+		this.particles.init(this.samples);
 	}
 
 	initThree() {
-		// scene
 		this.scene = new THREE.Scene();
-
-		// camera
 		this.camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
 		this.camera.position.z = 300;
-
-		// renderer
-        this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-
-        // clock
+		this.renderer = new THREE.WebGLRenderer({antialias: true, alpha: true});
 		this.clock = new THREE.Clock(true);
 	}
 
@@ -51,53 +29,33 @@ export default class WebGLView {
 		this.scene.add(this.particles.container);
 	}
 
-	// ---------------------------------------------------------------------------------------------
-	// PUBLIC
-	// ---------------------------------------------------------------------------------------------
-
 	update() {
 		const delta = this.clock.getDelta();
 
-		if (this.particles) this.particles.update(delta);
+		if (this.particles) {
+			this.particles.update(delta);
+		}
 	}
 
 	draw() {
 		this.renderer.render(this.scene, this.camera);
 	}
 
-
-	goto(index) {
-		// init next
-		if (this.currSample == null) this.particles.init(this.samples[index]);
-		// hide curr then init next
-		else {
-			this.particles.hide(true).then(() => {
-				this.particles.init(this.samples[index]);
-			});
+	resize() {
+		if (!this.renderer) {
+			return;
 		}
 
-		this.currSample = index;
-	}
-
-	next() {
-		if (this.currSample < this.samples.length - 1) this.goto(this.currSample + 1);
-		else this.goto(0);
-	}
-
-	// ---------------------------------------------------------------------------------------------
-	// EVENT HANDLERS
-	// ---------------------------------------------------------------------------------------------
-
-	resize() {
-		if (!this.renderer) return;
-		this.camera.aspect = window.innerWidth / window.innerHeight;
+		this.camera.aspect = document.querySelector('.container').offsetWidth / window.innerHeight;
 		this.camera.updateProjectionMatrix();
-
 		this.fovHeight = 2 * Math.tan((this.camera.fov * Math.PI) / 180 / 2) * this.camera.position.z;
+		this.renderer.setSize(document.querySelector('.container').offsetWidth, window.innerHeight);
 
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
-
-		if (this.interactive) this.interactive.resize();
-		if (this.particles) this.particles.resize();
+		if (this.interactive) {
+			this.interactive.resize();
+		}
+		if (this.particles) {
+			this.particles.resize();
+		}
 	}
 }
--- src/scripts/webgl/particles/TouchTexture.js
+++ src/scripts/webgl/particles/TouchTexture.js
@@ -1,15 +1,15 @@
 import * as THREE from 'three';
 
-import { easeOutQuad, easeInOutQuad, easeOutSine, easeInOutSine } from '../../utils/easing.utils';
+const easeOutSine = (t, b, c, d) => {
+	return c * Math.sin(t / d * (Math.PI / 2)) + b;
+};
 
 export default class TouchTexture {
-	constructor(parent) {
-		this.parent = parent;
+	constructor() {
 		this.size = 64;
 		this.maxAge = 120;
 		this.radius = 0.15;
 		this.trail = [];
-
 		this.initTexture();
 	}
 
@@ -19,29 +19,22 @@ export default class TouchTexture {
 		this.ctx = this.canvas.getContext('2d');
 		this.ctx.fillStyle = 'black';
 		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-
 		this.texture = new THREE.Texture(this.canvas);
-
 		this.canvas.id = 'touchTexture';
 		this.canvas.style.width = this.canvas.style.height = `${this.canvas.width}px`;
 	}
 
-	update(delta) {
+	update() {
 		this.clear();
-
-		// age points
 		this.trail.forEach((point, i) => {
 			point.age++;
-			// remove old
 			if (point.age > this.maxAge) {
 				this.trail.splice(i, 1);
 			}
 		});
-
-		this.trail.forEach((point, i) => {
+		this.trail.forEach((point) => {
 			this.drawTouch(point);
 		});
-
 		this.texture.needsUpdate = true;
 	}
 
@@ -53,13 +46,15 @@ export default class TouchTexture {
 	addTouch(point) {
 		let force = 0;
 		const last = this.trail[this.trail.length - 1];
+
 		if (last) {
-			const dx = last.x - point.x;
-			const dy = last.y - point.y;
-			const dd = dx * dx + dy * dy;
+			const dx = last.x - point.x,
+				dy = last.y - point.y,
+				dd = dx * dx + dy * dy;
 			force = Math.min(dd * 10000, 1);
 		}
-		this.trail.push({ x: point.x, y: point.y, age: 0, force });
+
+		this.trail.push({x: point.x, y: point.y, age: 0, force});
 	}
 
 	drawTouch(point) {
@@ -67,8 +62,8 @@ export default class TouchTexture {
 			x: point.x * this.size,
 			y: (1 - point.y) * this.size
 		};
-
 		let intensity = 1;
+
 		if (point.age < this.maxAge * 0.3) {
 			intensity = easeOutSine(point.age / (this.maxAge * 0.3), 0, 1, 1);
 		} else {
@@ -77,8 +72,9 @@ export default class TouchTexture {
 
 		intensity *= point.force;
 
-		const radius = this.size * this.radius * intensity;
-		const grd = this.ctx.createRadialGradient(pos.x, pos.y, radius * 0.25, pos.x, pos.y, radius);
+		const radius = this.size * this.radius * intensity,
+			grd = this.ctx.createRadialGradient(pos.x, pos.y, radius * 0.25, pos.x, pos.y, radius);
+
 		grd.addColorStop(0, `rgba(255, 255, 255, 0.2)`);
 		grd.addColorStop(1, 'rgba(0, 0, 0, 0.0)');
 
